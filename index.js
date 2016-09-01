84a05766abfaf7eb3515370cfbf1976f934fcc55
Sebastian Raff <sebastian.raff@tielsa.de>
2016-09-01 21:42:08 +0200

    restructured config

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ var app = express();
 
 module.exports = function (hmbridge) {
 
+    var that = this;
+
+
     var config = hmbridge.config.web || {};
 
     app.use(express.static(__dirname + '/www-src'));
+
 
     app.listen(config.port, function () {
         hmbridge.log.info('webserver listening on port', config.port);
@@ -32,6 +36,27 @@ module.exports = function (hmbridge) {
         res.send('ok');
         hmbridge.restart();
     });
+
+
+
+    app.get('/api/plugins', function (req, res) {
+
+
+        res.send(that.pluginList);
+    });
+
+    this.pluginList = [];
+
+    this.setPlugins = function (p) {
+        var plugins = Object.keys(p);
+        plugins.splice(plugins.indexOf('hmbridge-web'), 1);
+        plugins.forEach(function (plugin) {
+            that.pluginList.push({name: plugin, title: p[plugin].title});
+            app.use(express.static(__dirname + '../../' + plugin + '/www'));
+        });
+    }
+
+    return this;
 
 };
 
